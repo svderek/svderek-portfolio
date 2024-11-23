5e397f3300acd4d739fd1e348504cb1cf8bf1e31
ivndrkk <ivandereka19@gmail.com>
2024-11-23 06:24:30 +0000

    fix mob menu js

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const burgerButton = document.querySelector('.burger-button');
+    const burgerButton = document.querySelector('.burger');
     const closeButton = document.querySelector('.menu-icon-close');
     const mobileMenu = document.querySelector('.mob-menu');
     const menuLinks = document.querySelectorAll('.mob-nav-link');
