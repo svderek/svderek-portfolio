1eb63f06d73e4027cd1f1397dde5b460a81e8359
ivndrkk <ivandereka19@gmail.com>
2024-11-23 06:34:48 +0000

    fix logo and header nav

--- src/js/header.js
+++ src/js/header.js
@@ -1,3 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const aboutMeSection = document.querySelector('#about-me');
+    const projectsSection = document.querySelector('#projects');
+    const contactsSection = document.querySelector('#work-together');
 
-const burgerButton = document.querySelector('.burger');
-export { burgerButton };
+    const navLinks = document.querySelectorAll('.nav-link');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', event => {
+            event.preventDefault();
+        
+            if (link.textContent.includes('About Me')) {
+                aboutMeSection.scrollIntoView({ behavior: 'smooth' });
+            } else if (link.textContent.includes('Projects')) {
+                projectsSection.scrollIntoView({ behavior: 'smooth' });
+            } else if (link.textContent.includes('Contacts')) {
+                contactsSection.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
+    });
+});
