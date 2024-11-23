b667bac34d5dc06601f0b6c99965ffdb795f9df0
ivndrkk <ivandereka19@gmail.com>
2024-11-23 04:42:15 +0000

    add mobile menu

---
+++ src/js/mobile-menu.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const burgerButton = document.querySelector('.burger-button');
+    const closeButton = document.querySelector('.menu-icon-close');
+    const mobileMenu = document.querySelector('.mob-menu');
+    const menuLinks = document.querySelectorAll('.mob-nav-link');
+
+    function openMenu() {
+        mobileMenu.classList.add('is-open');
+        burgerButton.hidden = true;
+        closeButton.hidden = false;
+    }
+
+    function closeMenu() {
+        mobileMenu.classList.remove('is-open');
+        burgerButton.hidden = false;
+        closeButton.hidden = true;
+    }
+
+    burgerButton?.addEventListener('click', openMenu);
+
+    closeButton?.addEventListener('click', closeMenu);
+
+    menuLinks.forEach(link => {
+        link.addEventListener('click', closeMenu);
+    });
+});
