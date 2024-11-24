5209831a2655b312cc2bf0e126b5a6316af93e4f
ivndrkk <ivandereka19@gmail.com>
2024-11-24 05:44:26 +0000

    add mob menu animation

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,26 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const burgerButton = document.querySelector('.burger');
-    const closeButton = document.querySelector('.menu-icon-close');
-    const mobileMenu = document.querySelector('.mob-menu');
-    const menuLinks = document.querySelectorAll('.mob-nav-link');
+  const burgerButton = document.querySelector('.burger');
+  const closeButton = document.querySelector('.menu-icon-close');
+  const mobileMenu = document.querySelector('.mob-menu');
+  const menuLinks = document.querySelectorAll('.mob-nav-link');
 
-    function openMenu() {
-        mobileMenu.classList.add('is-open');
-        burgerButton.hidden = true;
-        closeButton.hidden = false;
-    }
+  function openMenu() {
+    mobileMenu.classList.add('is-open');
+    burgerButton.hidden = true;
+    closeButton.hidden = false;
+  }
 
-    function closeMenu() {
-        mobileMenu.classList.remove('is-open');
-        burgerButton.hidden = false;
-        closeButton.hidden = true;
-    }
+  function closeMenu() {
+    mobileMenu.classList.remove('is-open');
+    burgerButton.hidden = false;
+    closeButton.hidden = true;
+  }
 
-    burgerButton?.addEventListener('click', openMenu);
+  burgerButton?.addEventListener('click', openMenu);
 
-    closeButton?.addEventListener('click', closeMenu);
+  closeButton?.addEventListener('click', closeMenu);
 
-    menuLinks.forEach(link => {
-        link.addEventListener('click', closeMenu);
-    });
+  menuLinks.forEach(link => {
+    link.addEventListener('click', closeMenu);
+  });
 });
